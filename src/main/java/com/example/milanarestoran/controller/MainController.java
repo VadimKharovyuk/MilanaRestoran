f4c1dbe4fb99fc37ca614c828158c4313c5644da
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-07-05 03:50:02 +0300

    add  list

--- src/main/java/com/example/milanarestoran/controller/MainController.java
+++ src/main/java/com/example/milanarestoran/controller/MainController.java
@@ -1,15 +1,23 @@
 package com.example.milanarestoran.controller;
 
+import com.example.milanarestoran.service.CategoryService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class MainController {
+    private final CategoryService categoryService;
 
 
 
     @GetMapping("/")
-    public String homePage(){
+    public String homePage(Model model){
+
+       model.addAttribute("categories", categoryService.getAllCategories());
+
         return "HomePage";
     }
 }
